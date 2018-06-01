ab04d50aee10ea2dadcd17ba6efabf24206ae8cc
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-31 22:38:25 -0500

    LevelDB put y get usando Futures

--- src/leveldb.dart
+++ src/leveldb.dart
@@ -2,13 +2,44 @@ import 'dart:async';
 import 'package:leveldb/leveldb.dart';
 
 void main(){
-  print("main");
-  database();
+  consultar();
 }
 
+void insertar(){
+  set('apellido', 'valdivia').then((Null){
+    print('insertó');
+  }).catchError((e){
+    print(e);
+  });
+}
+
+void consultar(){
+  get('apellidos').then((data){
+    print('1 then');
+    print(data);
+    print('2 then');
+  }).catchError((e){
+    print(e);
+  });
+}
+
+String dbPath = '/home/pepe/Documentos/dart/aprendiendo/db/level.db';
+
+/*
 Future<dynamic> database() async {
-  LevelDB<String, String> db = await LevelDB.openUtf8("/home/pepe/Documentos/dart/aprendiendo/db/level.db");
+  LevelDB<String, String> db = await LevelDB.openUtf8(dbPath);
   //db.put("abc", "def");
   String value = db.get("abc");
   print("value is $value"); // value2 is def
+}
+*/
+
+Future<String> get(String key) async {
+  LevelDB<String, String> db = await LevelDB.openUtf8(dbPath);
+  return db.get(key);
+}
+
+Future<Null> set(String key, String value) async {
+  LevelDB<String, String> db = await LevelDB.openUtf8(dbPath);
+  db.put(key, value);
 }
